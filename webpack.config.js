b11e1e9425a11154a2363c4c1a1fc9ed6d57cfce
martin byrne <marb61a@gmail.com>
2016-08-02 10:45:29 +0000

    updated  webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,37 @@ module.exports = {
     output : {
         path: __dirname,
         filename: './public/bundle.js'    
-    }
+    },
+    
+    resolve : {
+        root : __dirname,
+        modulesDirectories : [
+            'node_modules',
+            './app/components',
+            './app/api'    
+        ],
+        alias :{
+            applicationStyles: 'app/styles/app.scss'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    
+    module : {
+        loaders : [{
+            loader : 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-0']
+            },
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/
+        }]
+    },
+    
+    sassLoader : {
+        includePaths : [
+            path.resolve(__dirname, './node_modules/foundation-sites/scss')    
+        ]
+    },
+    
+    devtool : 'cheap-module-eval-source-map'
 };
