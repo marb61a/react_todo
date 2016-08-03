775807da0a05ef4f1c6a7aec18ea3ae572bb322f
martin byrne <marb61a@gmail.com>
2016-08-03 10:43:41 +0000

    added initial todo and todoapp component files

---
+++ app/components/Todo.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+var moment = require("moment");
+
+var Todo = React.createClass({
+    render : function(){
+        
+        return(
+            <div></div>    
+        );
+    }
+});
+
+module.exports = Todo;
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
+var TodoAPI = require('TodoAPI');
+
+var TodoApp = React.createClass({
+    
+});
+
+module.exports = TodoApp;
