b71ef4a4f68476c61a21cb791c918d2d5ddafaa1
martin byrne <marb61a@gmail.com>
2016-08-08 13:02:52 +0000

    added settodos test in todoapitest

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,4 +11,26 @@ describe('TodoAPI', () => {
         expect(TodoAPI).toExist();
     });
     
+    describe('setTodos', () => {
+        it('should set valid todos array', () => {
+            var todos = [{
+                id : 23,
+                test: 'test all files',
+                completed : false
+            }];
+            TodoAPI.setTodos(todos);
+            
+            var actualTodos = JSON.parse(localStorage.getItem('todos'));
+            
+            expect(actualTodos).toEqual(todos);
+        });
+        
+        it('should not set invalid todos array', () => {
+            var badTodos = {a: 'b'};
+            TodoAPI.setTodos(badTodos);
+            
+            expect(localStorage.getItem('todos')).toBe(null);
+        });
+    });
+    
 })
