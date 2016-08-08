20b46ad302f995606e00ae69161a35d5c5fcdd07
martin byrne <marb61a@gmail.com>
2016-08-08 12:26:24 +0000

    added filtertodos function to todoapi

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,5 +17,35 @@ module.exports = {
         } catch (e){
             todos = JSON.parse(stringTodos);
         }
+        
+        return $.isArray(todos) ? todos : [];
+    },
+    
+    filterTodos : function(todos, showCompleted, searchText){
+        var filteredTodos = todos;
+        
+        // Filter todos by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;    
+        });
+        
+        // Filter todos by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+        
+        // Sort the todos with the non completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }    
+        });
+        
+        return filteredTodos;
     }
 };
