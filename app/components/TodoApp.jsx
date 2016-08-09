22b95cc804d3d9477652c50f10110cedeee2c3df
martin byrne <marb61a@gmail.com>
2016-08-09 10:35:42 +0000

    updated todoapp

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -35,11 +35,42 @@ var TodoApp = React.createClass({
         });  
     },
     
+    handleToggle : function(id){
+        var updatedTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
+            }
+            
+            return todo;
+            });
+        
+        this.setState({todos: updatedTodos});        
+    },
+    
     handleSearch : function(showCompleted, searchText){
         this.setState({
            showCompleted : showCompleted,
            searchText: searchText.toLowerCase()
         });
+    },
+    
+    render : function(){
+        var {todos, showCompleted, searchText} = this.state;
+        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+        
+        return(
+            <div>
+                <h1 className="page-title">Todo App</h1>
+                <div className="column small-centered small-11 medium-6 large-5">
+                    <div className="container">
+                        <TodoSearch onSearch={this.handleSearch}/>
+                        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+                        <AddTodo onAddTodo={this.handleAddTodo}/>
+                    </div>
+                </div>
+            </div>    
+        );
     }
 });
 
